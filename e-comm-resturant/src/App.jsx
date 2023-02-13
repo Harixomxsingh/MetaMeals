c4a298cd4d79af0b843ef6385b7589b9f25c2489
Hari om Singh <www.hariom03.com@gmail.com>
2023-02-13 13:54:50 +0530

    initalize and create header component

---
+++ e-comm-resturant/src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import "./App.css";
+import Header from "./components/Layout/Header/Header";
+
+function App() {
+  return (
+    <Fragment className="app">
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
